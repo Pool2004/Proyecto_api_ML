ecb76451141a2622b01ab993b9e69e5eb5fea243
Jean Paul Ordoñez Ibarguen <jeandeveloper04@gmail.com>
2025-12-05 21:53:00 -0500

    Update dataset path in train_yolo.py

--- src/train_yolo.py
+++ src/train_yolo.py
@@ -3,7 +3,7 @@ from ultralytics import YOLO
 model = YOLO("yolov8n-pose.pt")  # o yolov9c-pose.pt
 
 model.train(
-    data="dataset.yaml",
+    data="../dataset.yaml",
     imgsz=640,
     epochs=100,
     batch=16,
